74f931fc6442efb1cf00d10766821b866d7bda09
= <=>
2022-11-28 14:14:51 +0800

    Making OWL components reactive

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # odoo15
 Working on my Odoo cookbook
 
-I jumped to Chp 16 Owl because of the large changes to web dev in Odoo to Owl.
-Creating an OWL component
-1.  I do not use the template.xml in views dir.
-2.  I used the manifest.py to add the component.js file to the assets_backend
-3.  It seems to work, except the component was loaded on top instead of on the bottom.  Not sure why.
-
-Managing user actions in an OWL component
-We add a 't-on-<event>' to response to events.
-In this example, the event is a 'click'.  It could have been a mouseover.
-Triggering will call the method 'OnRemove' which is calling the default this.destroy() fn.
+Making OWL components reactive
+The previous exercise was to react to events.  this is to react to changes in the state of the component.
--- local-addons/my_library/static/src/js/component.js
+++ local-addons/my_library/static/src/js/component.js
@@ -1,19 +1,51 @@
 odoo.define('my.component', function (require) {
     "use strict";
-
-    const { Component } = owl;
+    
+    const { Component, useState } = owl; // use State hook used
     const { xml } = owl.tags;
 
     class MyComponent extends Component {
         static template = xml`
             <div class="bg-info text-center p-2">
-                <b> Welcome to Odoo </b>
+
+                <i class="fa fa-arrow-left p-1"
+                    style="cursor: pointer;"
+                    t-on-click="onPrevious"> </i>
+
+                <b t-esc="messageList[Math.abs(state.currentIndex%4)]"/>
+
+                <i class="fa fa-arrow-right p1" 
+                    style="cursor: pointer;"
+                    t-on-click="onNext"> </i>
+
                 <i class="fa fa-close p-1 float-right"
                     style="cursor: pointer;"
                     t-on-click="onRemove"> </i>
+
             </div>`
+        // constructor to component and var
+        constructor() {
+            console.log('CALLED:> constructor');
+            super(...arguments);
+            this.messageList = [
+                    
+                    'Hello World',
+                    'Welcome to Odoo',
+                    'Odoo is awesome',
+                    'You are awesome too'
+            ];
+            // state.currentIndex%5:= the num '5' sets expected num of items in the list.  CurrentIndex: 1 is the start position of the list.
+            this.state = useState({ currentIndex: 1 });
+        }
+        // mtds to handle events
         onRemove(ev) {
             this.destroy();
+        }
+        onPrevious(ev) {
+            this.state.currentIndex--;
+        }
+        onNext(ev) {
+            this.state.currentIndex++;
         }
     }
 
